8b34647066dbfec9d2a31ccede1cdbc2a2f00165
Mubarak-Ansari <mubarakansari8948@gmail.com>
2026-05-01 22:31:23 +0530

    feat: Rest API Concept

---
+++ 09-Rest API in nodejs/index.js
@@ -0,0 +1,7 @@
+// What is REST API?
+// REST stands for Representational State Transfer. 
+// It is an architectural style for designing networked applications. 
+// A REST API (Application Programming Interface) is a set of rules and conventions for building and interacting with web services. It allows different software applications to communicate with each other over the internet using standard HTTP methods such as GET, POST, PUT, PATCH, and DELETE.
+
+// In a REST API, resources (such as users, products, or orders) are represented as URLs (Uniform Resource Locators). Clients can perform operations on these resources by sending HTTP requests to the appropriate URLs. The server processes the requests and returns responses, typically in JSON format, which can be easily consumed by clients.           
+// rest coming soon in next project
